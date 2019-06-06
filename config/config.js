aa2606060091152e421b41539699403e87b13068
mojtaba safavi <sms7182@gmail.com>
2019-06-06 17:17:55 +0430

    change source :

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports={
+    production:{
+        username:'sa',
+        password:'sa123',
+        database:'TestDB',
+        host:'localhost',
+        dialect:'mssql', 
+        use_env_variable: 'DATABASE_URL'
+    }
+}
+
