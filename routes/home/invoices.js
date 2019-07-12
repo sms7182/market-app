2df66261d0c57bb2acaa5f934aa0e05893710569
sms7182 <sms7182@gmail.com>
2019-07-12 17:01:02 +0430

    fix error and update for invoice and lines working true

--- routes/home/invoices.js
+++ routes/home/invoices.js
@@ -16,45 +16,72 @@ router.all('/*',(req,res,next)=>{
 
 router.post('/edit',(req,res)=>{
    
-
     if(req.body){
-        Invoice.findOne({_id:req.body.invoiceId},function(err,obj){
-            console.log('loading ...')
+        Invoice.findOne({_id:req.body.invoiceId}).populate('invoiceLines').exec(function(err,obj){
+            console.log('loading ...');
+            
             if(err){
                 console.log(err);
                res.send(err);    
             }
             if(obj&&obj!=null){
-                console.log('start edit invoice')
-              obj= createInvoice(obj,req.body);
-              console.log('invoice '+ req.body.invoiceLines[0].code);
+              console.log('start edit invoice')
+              obj= createOrEditInvoice(obj,req.body);
+              console.log(obj.invoiceLines);
               
-              obj.save().then(s=>{
-                console.log('edit saving is working')
-                
-            }).catch(err=>res.status(400).send(`could not save because: ${err}`));
-             Invoice.findOne({_id:req.body.invoiceId}).populate('invoiceLines')
-          
-             .exec(function(err,invLine){
-               
-                if(invLine){
-                    console.log(invLine);
-                 }
-             });
+             obj.save();
+         let cnt=0;
+         cnt=obj.invoiceLines.length;
+              for(var i=0;i<req.body.invoiceLines.length;i++){
+                if(req.body.invoiceLines[i].id&&req.body.invoiceLines[i].id!=null){
+                InvoiceLine.findOneAndUpdate({_id:req.body.invoiceLines[i].id},{$set:{
+                    totalPrice:req.body.invoiceLines[i].totalPrice,
+                    decPrice:req.body.invoiceLines[i].decPrice,
+                    incPrice:req.body.invoiceLines[i].incPrice,
+                    netPrice:req.body.invoiceLines[i].netPrice,
+                }},{new:true},function(err,doc){
+                    if(err){
+                        console.log('update line with id: '+req.body.invoiceLines[i].id+' has error '+err);
+                    }
+                })
+                console.log('updated line with id:'+req.body.invoiceLines[i].id);
+                }
+                else{
+                    console.log(obj);
+                var invoiceLine=new InvoiceLine();
+                  invoiceLine.code=req.body.invoiceLines[i].code;
+                  invoiceLine.rowOrder=cnt;
+                  invoiceLine.totalPrice=req.body.invoiceLines[i].totalPrice;
+                  invoiceLine.quantity=req.body.invoiceLines[i].quantity;
+                  invoiceLine.decPrice=req.body.invoiceLines[i].decPrice;
+                  invoiceLine.incPrice=req.body.invoiceLines[i].incPrice;
+                  invoiceLine.netPrice=req.body.invoiceLines[i].netPrice;
+                  invoiceLine.title=req.body.invoiceLines[i].title;    
+                 cnt=cnt+1;
+                  obj.invoiceLines.push(invoiceLine);
+                  invoiceLine.invoice=obj._id;
+                  invoiceLine.save(function(err){
+                       if(err){
 
+                           console.log(err);
+                           res.send('Error in line saving: '+err);
+                       }
+                    });
+                 }
+                }
+             
+              res.send(obj._id);
             }
-            else{
+     
+     else{
                 console.log('start create invoice');
                var temp= new Invoice();
 
-              temp= createInvoice(temp,req.body);
+              temp= createOrEditInvoice(temp,req.body);
           
               temp.save(function(err){
-                  console.log('create saving is working');
-                  console.log(req.body.invoiceLines.length);
-                  
-
-                  res.send(temp._id);
+                 console.log(err);
+                 res.send(err);
               });
               for(var i=0;i<req.body.invoiceLines.length;i++){
                 var invoiceLine=new InvoiceLine();
@@ -63,41 +90,31 @@ router.post('/edit',(req,res)=>{
                   invoiceLine.decPrice=req.body.invoiceLines[i].decPrice;
                   invoiceLine.incPrice=req.body.invoiceLines[i].incPrice;
                   invoiceLine.netPrice=req.body.invoiceLines[i].netPrice;
-                  invoiceLine.title=req.body.invoiceLines[i].title;          
-                  console.log('before push')
+                  invoiceLine.title=req.body.invoiceLines[i].title;  
+                  invoiceLine.rowOrder=i;        
                   temp.invoiceLines.push(invoiceLine);
-                  console.log('after push')
                   invoiceLine.invoice=temp._id;
-                    invoiceLine.save(function(err){
-                        console.log(err+'invoice line is saved');
+                  invoiceLine.save(function(err){
+                       if(err){
+
+                           console.log(err);
+                           res.send('Error in line saving: '+err);
+                       }
                     });
                  }
-                 temp.save(function(err){
-                     console.log('after push saving')
-                 })
+                
             }
             
-        })
-            
-
-
-           /* console.log(req.body);
-            invoice.save().then(()=>{
-                console.log('invoice saved');
-            }).catch(err=>{
-                res.send(err);
-            })*/
-    }
-    
+        });
+    } 
 });
 
-
-function createInvoice(invoiceInstance,reqbody){
+function createOrEditInvoice(invoiceInstance,reqbody){
    var invoice=invoiceInstance;
  
     
     invoice.slug=reqbody.code;
-   // invoice.store=req.body.store.id;
+  
     invoice.code=reqbody.code;
     invoice.status=reqbody.status;
     invoice.netPrice=reqbody.netPrice;
@@ -105,19 +122,10 @@ function createInvoice(invoiceInstance,reqbody){
     invoice.incPrice=reqbody.incPrice;
     invoice.store=reqbody.store;
     invoice.totalPrice=reqbody.totalPrice;
-    var index=1;
-   
-    
-    
-    console.log('invoice saved')
-
   return invoice;
-
-
-
 
 }
 
-
+    
 
 module.exports = router;
