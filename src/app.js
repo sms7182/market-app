69e18a1eacccbd16097226ae47a8e7e813676aea
Shabnam Ranjgar <shabnam.ranjgar@live.com>
2019-07-19 17:55:50 +0430

    SSL

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
+const https=require('https');
+const fs= require('fs');
+const path=require('path');
 const app = require('./appCreate');
 const port=process.env.PORT || 7575;
 console.log(process.env.PORT );
-// console.log(process.env.MONGODB_URL);
+console.log(process.env.MONGODB_URL);
 
-app.listen(port,()=>{
-    console.log('Server is running on 7575');
-});
+const options = {
+    key:fs.readFileSync(path.join(__dirname, '../config/marketApp-key.pem')),
+    cert:fs.readFileSync(path.join(__dirname, '../config//marketApp-chain.pem'))
+};
+
+// app.listen(port,()=>{
+//     console.log('Server is running on 7575');
+// });
+
+https.createServer(options,app).listen(port);
